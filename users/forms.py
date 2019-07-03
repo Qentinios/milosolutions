86f5d431ce984b697cebc44d41e0d33c0ce583f5
Adrian Czok <qentinios@gmail.com>
2019-07-03 13:14:36 +0200

    Bithday form field as date, password field as password input

--- users/forms.py
+++ users/forms.py
@@ -4,6 +4,14 @@ from users import models
 
 
 class MiloUserForm(forms.ModelForm):
+    birthday = forms.DateField(
+        widget=forms.TextInput(
+            attrs={'type': 'date'}
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput()
+    )
 
     class Meta:
         exclude = ['random', 'date_joined', 'last_login']
