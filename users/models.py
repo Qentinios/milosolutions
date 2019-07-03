55b0b4ff4bcacb4bc2fe7ba4c842d4418d2d4bee
Adrian Czok <qentinios@gmail.com>
2019-07-03 12:07:56 +0200

    Added users app. Created MiloUser with new fields, bithday and random

---
+++ users/models.py
@@ -0,0 +1,12 @@
+import random
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+def random_int():
+    return random.randint(1, 100)
+
+
+class MiloUser(AbstractUser):
+    birthday = models.DateField(null=True)
+    random = models.IntegerField(editable=False, default=random_int())
